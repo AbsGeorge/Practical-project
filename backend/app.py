ee1cc04cc37d235680f8422cc8d8f523106b10d9
Henil <henil.soni13@gmail.com>
2021-05-29 16:33:10 +0000

    Two-server-all-backendlogic-added

--- backend/app.py
+++ backend/app.py
@@ -10,29 +10,62 @@ Bundesliga = ['Bayern Munich', 'RB Leipzig', 'Dortmund', 'Wolfsburg']
 SerieA = ['Inter Milan', 'AC Milan', 'Atalanta', 'Juventus']
 Ligue1 = ['Lille', 'PSG', 'Monaco', 'Lyon']
 
+
 @app.route('/get_country', methods=['GET'])
 def get_country():
     return random.choice(country_list)
 
 @app.route('/get_match', methods=['POST'])
 def get_match():
+    player1_number = random.randint(1, 50)
+    player2_number = random.randint(1, 50)
     country = request.data.decode('utf-8')
     if country == 'England':
-        team1 = random.choice(Premierleague)
-        team2 = random.choice(Premierleague)
+        if player1_number % 10 == 0:
+            team1 = "Classic XI"         
+        else:
+            team1 = random.choice(Premierleague)
+        if player2_number % 10 == 0:
+            team2 = "Classic XI"         
+        else:
+            team2 = random.choice(Premierleague)
     elif country == 'Spain':
-        team1 = random.choice(LaLiga)
-        team2 = random.choice(LaLiga)
+        if player1_number % 10 == 0:
+            team1 = "Classic XI"         
+        else:
+            team1 = random.choice(LaLiga)
+        if player2_number % 10 == 0:
+            team2 = "Classic XI"         
+        else:
+            team2 = random.choice(LaLiga)
     elif country == 'Germany':
-        team1 = random.choice(Bundesliga)
-        team2 = random.choice(Bundesliga)
+        if player1_number % 10 == 0:
+            team1 = "Classic XI"         
+        else:
+            team1 = random.choice(Bundesliga)
+        if player2_number % 10 == 0:
+            team2 = "Classic XI"         
+        else:
+            team2 = random.choice(Bundesliga)
     elif country == 'Italy':
-        team1 = random.choice(SerieA)
-        team2 = random.choice(SerieA)
+        if player1_number % 10 == 0:
+            team1 = "Classic XI"         
+        else:
+            team1 = random.choice(SerieA)
+        if player2_number % 10 == 0:
+            team2 = "Classic XI"         
+        else:
+            team2 = random.choice(SerieA)
     elif country == 'France':
-        team1 = random.choice(Ligue1)
-        team2 = random.choice(Ligue1)
-    return f"the country is: {country} and the matchup: {team1} vs {team2}"       
+        if player1_number % 10 == 0:
+            team1 = "Classic XI"         
+        else:
+            team1 = random.choice(Ligue1)
+        if player2_number % 10 == 0:
+            team2 = "Classic XI"         
+        else:
+            team2 = random.choice(Ligue1)
+    return f"player 1 number: {player1_number}, player 2 number: {player2_number} the country is: {country} and the matchup: {team1} vs {team2}"       
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
