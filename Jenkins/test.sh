9999f09f9ff66a4a4aa83feac36b49476ca47707
Henil <henil.soni13@gmail.com>
2021-06-03 13:19:07 +0000

    jenkinsfile for testing

---
+++ Jenkins/test.sh
@@ -0,0 +1,22 @@
+# create and activate venv
+python3 -m venv venv
+source venv/bin/activate
+
+# install dependencies
+pip3 install pytest
+pip3 install requests-mock
+pip3 install flask
+pip3 install flask-sqlalchemy
+pip3 install flask-testing 
+
+#test frontend 
+python3 -m pytest frontend 
+
+#test backend 
+python3 -m pytest backend 
+
+#test backend-country
+python3 -m pytest backend-country
+
+#test backend-numbers
+python3 -m pytest backend-numbers
