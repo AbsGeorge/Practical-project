cfe1f7cba7503c268634f5ab3427bf656e2511a4
Henil <henil.soni13@gmail.com>
2021-06-04 14:30:13 +0000

    docker-setup added

--- jenkins/setup-docker.sh
+++ jenkins/setup-docker.sh
@@ -7,7 +7,7 @@ curl https://get.docker.com | sudo bash
 
 #install docker-compose
 sudo usermod -aG docker $(whoami)
-newgrp docker 
+ 
 sudo apt install -y curl jq
 version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
